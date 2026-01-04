aed18c74d6a93aa16e15260430bc46b0855ea2ac
tykim0507 <nowtyk@snu.ac.kr>
2026-01-04 16:40:55 +0900

    Make bc not save wandb code to prevent memory issues

--- src/train/bc.py
+++ src/train/bc.py
@@ -371,6 +371,7 @@ def main(cfg: DictConfig):
         config=config_dict,
         mode=cfg.wandb.mode,
         notes=cfg.wandb.notes,
+        save_code=False,
     )
 
     if cfg.wandb.watch_model:
@@ -591,7 +592,7 @@ def main(cfg: DictConfig):
                 best_test_loss = test_loss_mean
                 save_path = str(model_save_dir / f"actor_chkpt_best_test_loss.pt")
                 torch.save(save_dict, save_path)
-                wandb.save(save_path)
+                # wandb.save(save_path)
 
             # Save the model if the success rate is the best so far
             if (
@@ -601,7 +602,7 @@ def main(cfg: DictConfig):
                 prev_best_success_rate = best_success_rate
                 save_path = str(model_save_dir / f"actor_chkpt_best_success_rate.pt")
                 torch.save(save_dict, save_path)
-                wandb.save(save_path)
+                # wandb.save(save_path)
 
             if (
                 cfg.training.checkpoint_interval > 0
@@ -609,7 +610,7 @@ def main(cfg: DictConfig):
             ):
                 save_path = str(model_save_dir / f"actor_chkpt_{epoch_idx}.pt")
                 torch.save(save_dict, save_path)
-                wandb.save(save_path)
+                # wandb.save(save_path)
 
             # Run diffusion sampling on a training batch
             if (
@@ -649,7 +650,7 @@ def main(cfg: DictConfig):
         if cfg.training.store_last_model:
             save_path = str(model_save_dir / f"actor_chkpt_last.pt")
             torch.save(save_dict, save_path)
-            wandb.save(save_path)
+            # wandb.save(save_path)
 
         # If switch is enabled, copy the the shadow to the model at the end of each epoch
         if cfg.training.ema.use and cfg.training.ema.switch:
